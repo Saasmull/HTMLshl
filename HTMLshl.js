fa0cdd760e5acbf77e9856dd9d236dd63889d5f6
Saasmull <79657472+Saasmull@users.noreply.github.com>
2022-03-14 18:15:48 +0100

    Update HTMLshl.js

--- HTMLshl.js
+++ HTMLshl.js
@@ -1,1 +1,9 @@
-
+export class HTMLshl{
+    static highlight(htmlString){
+        return "<pre>" + htmlString.replaceAll(/(<(.*?)>)/gmis, function(a, m, p1){
+            return "<span class=\"tag\">&lt;" + p1.replaceAll(/(([A-z]*\=)\"([^"]*)\")/gmis, function(a, m, p1, p2){
+                return "<span class=\"attribute\">" + p1 + "\"<span class=\"property\">" + p2 + "</span>\"</span>";
+            }) + "&gt;</span>";
+        }) + "</pre>";
+    }
+}
