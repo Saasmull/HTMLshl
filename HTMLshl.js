afea50c790d9897d7caf8373efa3f95edaa1e000
Saasmull <79657472+Saasmull@users.noreply.github.com>
2022-03-15 18:37:42 +0100

    Added comment support

--- HTMLshl.js
+++ HTMLshl.js
@@ -1,9 +1,34 @@
 export class HTMLshl{
     static highlight(htmlString){
+        var c = false;
         return "<pre>" + htmlString.replaceAll(/(<(.*?)>)/gmis, function(a, m, p1){
-            return "<span class=\"tag\">&lt;" + p1.replaceAll(/(([A-z]*\=)\"([^"]*)\")/gmis, function(a, m, p1, p2){
-                return "<span class=\"attribute\">" + p1 + "\"<span class=\"property\">" + p2 + "</span>\"</span>";
-            }) + "&gt;</span>";
+            if(p1?.startsWith("!--") && !c){
+                if(p1?.length > 5 && p1?.endsWith("--")){
+                    return "<span class=\"comment\">&lt;"+p1+"&gt;</span>";
+                }else{
+                    c = true;
+                    return "<span class=\"comment\">&lt;"+p1+"&gt;";
+                }
+            }else if(p1?.endsWith("--")){
+                c = false;
+                return "&lt;"+p1+"&gt;</span>";
+            }else if(!c){
+                return "<span class=\"tag\">&lt;" + p1.replaceAll(/(\r\n|\r|\n|\s| )([^\=\s]+)(\=\"[^\"]*\"|\=\'[^\']*\'|\=[^\"\'][\w\"\'\-\*\,\.;\:\!\?\^\°\#\+\$\§\%\&\/\(\)\{\}\[\]\=\~\<\>\|\@]*(?=[\r\n|\r|\n|\s| |$])|)/gmis, function(a, m, p1, p2){
+                    if(p2){
+                        if(p2?.startsWith("=\"") && p2?.endsWith("\"")){
+                            return m + "<span class=\"attribute\">" + p1 + "=\"<span class=\"property\">" + p2.substr(2).slice(0, -1) + "</span>\"</span>";
+                        }else if(p2?.startsWith("=\'") && p2?.endsWith("\'")){
+                            return m + "<span class=\"attribute\">" + p1 + "=\'<span class=\"property\">" + p2.substr(2).slice(0, -1) + "</span>\'</span>";
+                        }else{
+                            return m + "<span class=\"attribute\">" + p1 + "=<span class=\"property\">" + p2 .substr(1)+ "</span></span>";
+                        }
+                    }else{
+                        return m + "<span class=\"attribute\">" + p1 + "</span>";
+                    }
+                }) + "&gt;</span>";
+            }else{
+                return "&lt;"+p1+"&gt;";
+            }
         }) + "</pre>";
     }
 }
